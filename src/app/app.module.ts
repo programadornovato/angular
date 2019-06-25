b3d8cfbd482ee12788fc8d9791e3333e520d93b5
Programador Novato <info@programadornovato.com>
2019-06-24 23:06:48 -0500

    Angular🔺 js [09.-Crear nuestro propio componente]
    https://youtu.be/Q5sMC7CqPpA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { PieComponent } from './pie/pie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    PieComponent
   ],
   imports: [
     BrowserModule,
