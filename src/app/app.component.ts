f828df1356b85e239c8d70a519b3269410a3bd00
Programador Novato <info@programadornovato.com>
2019-06-26 01:44:11 -0500

    Angular 🔺[13.-NgFor (NgForOf)]
    https://youtu.be/4KPyvAhhCrc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'programadornovato.com';
   mostrar=true;
+  lenguajes:string[]=['JS','PHP','Java'];
+  frameworks:Array<any>=[
+    {nombre:'Angular',pop:'70%',edad:9},
+    {nombre:'Reactjs',pop:'90%',edad:5},
+    {nombre:'Vuejs',pop:'20%',edad:3}
+  ];
 }
